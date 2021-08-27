cb5aa4ad9a9150cc3c8abb9c2372402c6cd8e529
meelen1 <monkhbold.meelen@gmail.com>
2021-08-27 17:18:39 +0800

    app.js updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var uiController = (function () {
     },
   };
 })();
-
+var find = (function () {})();
 var financeController = (function () {})();
 
 var appController = (function (uiController, fnController) {
