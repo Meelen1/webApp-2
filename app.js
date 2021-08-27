b2be63abc2ea43a80fcd3ad5ab1e825e4bdff616
meelen1 <monkhbold.meelen@gmail.com>
2021-08-27 17:10:37 +0800

    architectur

---
+++ app.js
@@ -0,0 +1,33 @@
+var uiController = (function () {
+  var x = 100;
+
+  function add(y) {
+    return x + y;
+  }
+
+  return {
+    publicAdd: function (a) {
+      a = add(a);
+      console.log("боловсруулсан утга : " + a);
+    },
+  };
+})();
+
+var financeController = (function () {})();
+
+var appController = (function (uiController, fnController) {
+  uiController.publicAdd(50);
+})(uiController, financeController);
+
+var hunController = (function () {
+  var bodol = "Javascript толгой эргүүлмээр юм";
+  function tsusGuih() {}
+
+  function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
+
+  return {
+    yarih: function () {
+      console.log("hi");
+    },
+  };
+})();
