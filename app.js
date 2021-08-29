df8c78ed52b89611213305e41eb9b0b9803bbf4b
meelen1 <monkhbold.meelen@gmail.com>
2021-08-29 17:10:13 +0800

    Dom

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 //Дэлгэцтэй ажиллах контоллер
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 //санхүүтэй ажиллах контроллер
 var financeController = (function () {})();
 
 //Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function () {
     //1.оруулах өгөгдлийг дэлгэцнээс олжавна.
-    console.log("дэлгэцнээс өгөгдөл авах хэсэг");
+    console.log(uiController.getInput());
     //2. олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     //3.олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     //4. төсвийг тооцоолно.
     //5. эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
