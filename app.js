31a3fcbd1a6cde78bfcdf4ab2c3b4ddd922d32a1
meelen1 <monkhbold.meelen@gmail.com>
2021-08-31 23:36:02 +0800

    finance

--- app.js
+++ app.js
@@ -23,12 +23,33 @@ var uiController = (function () {
 })();
 
 //санхүүтэй ажиллах контроллер
-var financeController = (function () {})();
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+})();
 
 //Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
-  var DOM = uiController.getDOMstrings();
-
   var ctrlAddItem = function () {
     //1.оруулах өгөгдлийг дэлгэцнээс олжавна.
     console.log(uiController.getInput());
@@ -38,13 +59,26 @@ var appController = (function (uiController, financeController) {
     //5. эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
 
-  document.querySelector(DOM.addBtn).addEventListener("click", function () {
-    ctrlAddItem();
-  });
+  var setupEventListener = function () {
+    var DOM = uiController.getDOMstrings();
 
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListener();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
